c05f0c815a61ba9a2f3e46f2d479617313f1f459
sayan706 <sayanchatterjee706@gmail.com>
2024-05-20 20:41:42 +0530

    updated validation logic

--- app/serializers.py
+++ app/serializers.py
@@ -31,79 +31,78 @@ class Lung(serializers.Serializer):
 
   def validate_SMOKING(self,value):
     value = int(value)
-    if(value == 1 and value == 2):
+    if(value == 1 or value == 2):
       return value
-    raise ValueError("Invalid Input")
+    raise ValueError("Invalid Smoking Input")
 
   def validate_YELLOW_FINGERS(self,value):
     value = int(value)
-    if(value == 1 and value == 2):
+    if(value == 1 or value == 2):
       return value
-    raise ValueError("Invalid Input")    
+    raise ValueError("Invalid Yellow_Fingers Input")    
   
   def validate_ANXIETY(self,value):
     value = int(value)
-    if(value == 1 and value == 2):
+    if(value == 1 or value == 2):
       return value
-    raise ValueError("Invalid Input")
+    raise ValueError("Invalid ANXIETY Input")
   
   def validate_PEER_PRESSURE(self,value):
     value = int(value)
-    if(value == 1 and value == 2):
+    if(value == 1 or value == 2):
       return value
-    raise ValueError("Invalid Input")
+    raise ValueError("Invalid PEER_PRESSURE Input")
 
   def validate_CHRONIC_DISEASE(self,value):
     value = int(value)
-    if(value == 1 and value == 2):
+    if(value == 1 or value == 2):
       return value
-    raise ValueError("Invalid Input")
+    raise ValueError("Invalid CHRONIC_DISEASE Input")
   
   def validate_FATIGUE(self,value):
     value = int(value)
-    if(value == 1 and value == 2):
+    if(value == 1 or value == 2):
       return value
-    raise ValueError("Invalid Input")
+    raise ValueError("Invalid FATIGUE Input")
 
   def validate_ALLERGY(self,value):
     value = int(value)
-    if(value == 1 and value == 2):
+    if(value == 1 or value == 2):
       return value
-    raise ValueError("Invalid Input")
+    raise ValueError("Invalid ALLERGY Input")
   
   def validate_WHEEZING(self,value):
     value = int(value)
-    if(value == 1 and value == 2):
+    if(value == 1 or value == 2):
       return value
-    raise ValueError("Invalid Input")
+    raise ValueError("Invalid WHEEZING Input")
 
   def validate_ALCOHOL_CONSUMING(self,value):
     value = int(value)
-    if(value == 1 and value == 2):
+    if(value == 1 or value == 2):
       return value
-    raise ValueError("Invalid Input")
+    raise ValueError("Invalid ALCOHOL_CONSUMING Input")
 
   def validate_COUGHING(self,value):
     value = int(value)
-    if(value == 1 and value == 2):
+    if(value == 1 or value == 2):
       return value
-    raise ValueError("Invalid Input")
+    raise ValueError("Invalid COUGHING Input")
 
   def validate_SHORTNESS_OF_BREATH(self,value):
     value = int(value)
-    if(value == 1 and value == 2):
+    if(value == 1 or value == 2):
       return value
-    raise ValueError("Invalid Input")     
+    raise ValueError("Invalid SHORTNESS_OF_BREATH Input")     
 
   def validate_SWALLOWING_DIFFICULTY(self,value):
     value = int(value)
-    if(value == 1 and value == 2):
+    if(value == 1 or value == 2):
       return value
-    raise ValueError("Invalid Input")         
+    raise ValueError("Invalid SWALLOWING_DIFFICULTY Input")         
     
   def validate_CHEST_PAIN(self,value):
     value = int(value)
-    if(value == 1 and value == 2):
+    if(value == 1 or value == 2):
       return value
-    raise ValueError("Invalid Input")  
-  
+    raise ValueError("Invalid CHEST_PAIN Input")  
